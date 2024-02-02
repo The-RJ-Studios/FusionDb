493b1d5a86c97e13c325d70eb841b81631674fe6
The one and the only RJ <rjgamestar777@gmail.com>
2024-02-02 22:50:34 +0530

    Did some changes

---
+++ test/get.php
@@ -0,0 +1,6 @@
+<?php
+try {
+    header("Access-Control-Allow-Origin: 'https://anilist.co'");
+header("Referrer-Policy: no-referrer");
+header("Access-Control-Allow-Methods: 'GET, PUT, POST, DELETE, HEAD, OPTIONS'");
+}catch()
