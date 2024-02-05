34845d8fe51d0556e81d06bcbda2d1e03b5eb130
Namanshoo <namanshupardikar@gmail.com>
2024-02-05 14:09:24 +0530

    Fully Authentication
    Auth done cookie planting done
    
    Co-Authored-By: The one and the only RJ <114636049+Rj2537@users.noreply.github.com>
    Co-Authored-By: uzair777-dev <111451429+uzair777-dev@users.noreply.github.com>

--- pages/Users/script.js
+++ pages/Users/script.js
@@ -1,7 +1,7 @@
-const imageInput = document.getElementById('imageInput'); 
+// const imageInput = document.getElementById('imageInput'); 
      
-    imageInput.addEventListener('change', (event) => { 
-      const file = event.target.files[0]; 
-      // Perform further processing with the selected image file 
-      console.log(file); 
-    }); 
+//     imageInput.addEventListener('change', (event) => { 
+//       const file = event.target.files[0]; 
+//       // Perform further processing with the selected image file 
+//       console.log(file); 
+//     }); 
