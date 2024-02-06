bd61f8ac71b51b14d31c5893fee83b3b6682ab05
afnaan <afnaannim29@gmail.com>
2024-02-07 00:28:03 +0530

    poppu
    ubi

--- HomePage.js
+++ HomePage.js
@@ -1,22 +1,31 @@
-let counter = 1
+// let counter = 1
 
 
 
-setInterval(()=>{
+// setInterval(()=>{
 
-    document.querySelector('.sliderimg.show').classList.remove('show')
-    const img = document.querySelector(`.img-${counter}`)
-    img.classList.add('show')
-    counter++
+//     document.querySelector('.sliderimg.show').classList.remove('show')
+//     const img = document.querySelector(`.img-${counter}`)
+//     img.classList.add('show')
+//     counter++
 
-    if(counter >3){
-        counter = 1
-    }
+//     if(counter >3){
+//         counter = 1
+//     }
 
-},3000)
+// },3000)
+
+const btn = document.querySelector(".Nav_btn");
+btn.onmouseover = function(e){
+  const x = e.pageX - btn.offsetLeft;
+  const y = e.pageY - btn.offsetTop;
+
+  btn.computedStyleMap.setProperty("--x", x + "px");
+  btn.computedStyleMap.setProperty("--y", y + "px");
+}
 
 gsap.to("#nav", {
-    backgroundColor: "#5338AC",
+    backgroundColor: "transparent",
     duration: 0.5,
     height: "90px",
     scrollTrigger: {
@@ -28,3 +37,95 @@ gsap.to("#nav", {
       scrub: 1,
     },
   });
+
+function startLoader() {
+   let counterElement = document.querySelector(".counter");
+   let currentValue = 0;
+
+   function updateCounter(){
+    if(currentValue === 100){
+      return;
+    }
+
+    currentValue += Math.floor(Math.random() * 10) + 1;
+    
+    if (currentValue > 100){
+      currentValue = 100;
+    }
+
+    counterElement.textContext = currentValue;
+    let delay = Math.floor(Math.random() * 300) + 50; 
+    setTimeout(updateCounter, delay);
+  }
+
+  // var divToRemove = document.getElementById('mkc-2');
+  // if (divToRemove) {
+  // divToRemove.parentNode.removeChild(divToRemove); 
+  // }
+  // var TdivToRemove = document.getElementById('mkc');
+  // if (TdivToRemove) {
+  // TdivToRemove.parentNode.removeChild(TdivToRemove); 
+  // }
+  updateCounter();
+}
+
+startLoader();
+
+gsap.to(".counter",0.25,{
+  delay: 3.5,
+  opacity: 0,
+});
+gsap.to(".bar",1.5,{
+  delay: 3.5,
+  height: 0,
+  stagger: {
+    amount: 0.5,
+  },
+  ease: "power4.inOut",
+
+});
+gsap.from("#h1",1.5,{
+  delay: 4,
+  y: 700,
+  stagger: {
+    amount: 0.5,
+  },
+  ease : "power4.inOut",
+});
+gsap.from(".container",2,{
+  delay: 4.5,
+  y: 700,
+  ease : "power4.inOut",
+});
+setTimeout(() => {
+  var a = document.getElementById("mkc-2");
+  a.style.zIndex=-100;
+  var a = document.getElementById("mkc");
+  a.style.zIndex=-100; 
+}, 5000);
+
+
+// document.getElementById("page1").style.zIndex = 100000;
+
+// snap all of the properties in the comma-delimited list ("x,y" in this case) to the closest whole number:
+// gsap.to(".elem", {
+//   x: 100,
+//   y: 250,
+//   snap: "x,y",
+// });
+
+// // snap to an increment:
+// gsap.to(".elem-2", {
+//   x: 1000,
+//   snap: {
+//     x: 20, // x snaps to the closest increment of 20 (0, 20, 40, 60, etc.)
+//   },
+// });
+
+// // snap to the closest value in an array:
+// gsap.to(".elem-3", {
+//   x: 1000,
+//   snap: {
+//     x: [0, 50, 150, 500], // x snaps to the closest value in this array
+//   },
+// });
