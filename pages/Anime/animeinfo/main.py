f7ae09f3331c5a6f4dbb64145b07d0b2fbca688b
The one and the only RJ <rjgamestar777@gmail.com>
2024-02-07 22:02:32 +0530

    Cleanup

--- pages/Anime/animeinfo/main.py
+++ pages/Anime/animeinfo/main.py
@@ -1,27 +1,18 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+import pyjson5 as json5
+import urllib3
+import json 
 import requests
-import js
-import ssl
 from js import document, console
-# # from pyscript import Element
-# def extract_data_from_page(filename):
-#     s = BeautifulSoup(open(filename), "html.parser")
-head ='Loaded'
-try:
+try:   
+    urllib3.disable_warnings()
+    r = requests.get('https://imdb-api.uzairshaikhking777.workers.dev/title/tt2911666',verify= False)
+    console.log("Request sent")
+    p = json5.decode(r.read())
+    console.log(p)
+    parseR= json.loads(r)
+    console.log(parseR, "help")
     
-    print("Sending request")
-    r = requests.get('https://anilist.co/anime/918/Gintama/')
-    soup = BeautifulSoup(r.content, 'html.parser')
-    # print("Parsing the content")
-    # head = soup.find('h1').get_text() #finding h1 tag that contains heading and parsing it with get_text() funtion
-    # body = soup.find('p',class_='description').get_text()
-    # coverImgNP= soup.find('img',class_='cover')# Finding img 
-    # coverImg= coverImgNP['src'] #Finding src property from the tag
-    # bannerNP= soup.find('div',class_='banner')
-    # banner= bannerNP['style']
-    # print("Placing parsed content in the place")
-    document.getElementById("title").innerHTML = head
-    # console.log("Hello")
     
 except():
     print("Error encountered")
