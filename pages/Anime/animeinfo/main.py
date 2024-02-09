c13d15b6261bdb4707c4c517a93fdf9017754339
The one and the only RJ <rjgamestar777@gmail.com>
2024-02-09 23:35:45 +0530

    Did something
    Co-Authored-By: uzair777-dev <111451429+uzair777-dev@users.noreply.github.com>

--- pages/Anime/animeinfo/main.py
+++ pages/Anime/animeinfo/main.py
@@ -1,17 +1,19 @@
 # from bs4 import BeautifulSoup
+import json
 import urllib3
 import json 
 import requests
 from js import document, console
+
+def parser(data):
+    aData= list(data[0].key())
+    return aData
 try:   
     urllib3.disable_warnings()
     r = requests.get('https://imdb-api.uzairshaikhking777.workers.dev/title/tt2911666',verify= False)
     console.log("Request sent")
-    p = json5.decode(r.read())
-    console.log(p)
-    parseR= json.loads(r)
-    console.log(parseR, "help")
-    
+    print(r.json())
+    # json = r()[0]
     
 except():
     print("Error encountered")
