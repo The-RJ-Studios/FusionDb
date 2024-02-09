c7d01ff46c930dd1c252a3c6b8bed44343331539
The one and the only RJ <rjgamestar777@gmail.com>
2024-02-10 01:02:43 +0530

    Finally Displayed the data

--- pages/Anime/animeinfo/main.py
+++ pages/Anime/animeinfo/main.py
@@ -1,19 +1,33 @@
+# Importing needed things 
 # from bs4 import BeautifulSoup
-import json
 import urllib3
 import json 
 import requests
 from js import document, console
 
+# Generating Request Headers
+url ='https://imdb-api.uzairshaikhking777.workers.dev/title/tt0388629'
+userAgent ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3"
+req_header= {
+    'User-Agent' : userAgent,
+    'Accept-Language': 'en-US, en;q=0.5'
+}
+# The above lines should be as it is No changes should be made
 def parser(data):
     aData= list(data[0].key())
     return aData
+def getData():
+     res = requests.get(url, headers=req_header ,verify= False)
+     return res.json()
 try:   
     urllib3.disable_warnings()
-    r = requests.get('https://imdb-api.uzairshaikhking777.workers.dev/title/tt2911666',verify= False)
-    console.log("Request sent")
-    print(r.json())
-    # json = r()[0]
+    data= getData()
+    print(data['title'])
+    # console.log("Request sent")
+    document.getElementById('cover').src=data['image']
+    document.getElementById('title').innerHTML=data['title']
+    document.getElementById('info').innerHTML = data['plot']
+    document.getElementById('back-cover').src= data['images'][0]
     
 except():
     print("Error encountered")
