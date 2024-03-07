0d35a03a6960a55616fdcbbe09c197921d090f27
The one and the only RJ <rjgamestar777@gmail.com>
2024-03-07 10:19:16 +0530

    latest anime done
    Co-Authored-By: uzair777-dev <111451429+uzair777-dev@users.noreply.github.com>

--- pages/Anime/main2.py
+++ pages/Anime/main2.py
@@ -2,84 +2,66 @@
 # Json Comments
 # "beautifulsoup4",
 
-# import urllib3
-# import requests
+import urllib3
+import requests
 from js import document, console
 
 # Generating Request Headers
 # console.clear()
-latest =[{
-            "id":"154587",
-            "title":"Sousou no Frieren",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx154587-n1fmjRv4JQUd.jpg"
-        },{
-            "id":"161645",
-            "title":"Kusuriya no Hitorigoto",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx161645-7I8Cip7XRDhV.jpg"
-        },{
-            "id":"166216",
-            "title":"Boku no Kokoro no Yabai Yatsu 2nd Season",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx166216-mK2CNMDLsBfW.jpg"
-        },{
-            "id":"155227",
-            "title":"Kingdom 5th Season",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx155227-ZEAyBcZjSeet.jpg"
-        },{
-            "id":"21",
-            "title":"ONE PIECE",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx21-tXMN3Y20PIL9.jpg"
-        },{
-            "id":"151639",
-            "title":"Ninja Kamui",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx151639-7KqJ7BgaM9Mx.png"
-        },{
-            "id":"151807",
-            "title":"Solo Leveling",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx151807-m1gX3iwfIsLu.png"
-        },{
-            "id":"166794",
-            "title":"Yubisaki to Renren",
-            "image":"https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx166794-veSLL0zrrBhp.jpg"
-        }]
+
 # The above lines should be as it is No changes should be made
 # def parser(data):
 #     aData= list(data[0].key())
 #     return aData # Currently not doing anything
-
+url = 'https://fusiondb.pages.dev/pages/Anime/list.json'
+userAgent ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.3"
+req_header= {
+    'User-Agent' : userAgent,
+    'Accept-Language': 'en-US, en;q=0.5'
+}
+def getData(inurl):
+     res = requests.get(inurl, headers=req_header ,verify= False)
+     return res.json()
+i=1
 try:
 # Fetching data
-    rawData = latest
+    urllib3.disable_warnings()
+    data = getData(url)
+    rawData = data['latest']['results']
+    
     # Experimenting with creating a card div
     for data in rawData:
-        console.log('Creating item', i)
-        outerOuterDiv = document.getElementById('latest')
-        
-        outerDiv = document.createElement('div')
-        outerOuterDiv.appendChild(outerDiv)
-        outerDiv.className = "card popu"
-        
-        imageTag = document.createElement('img')
-        outerDiv.appendChild(imageTag)
-        imageTag.className="card-img"
-        imageTag.src= data['image']
-        
-        contentCard = document.createElement('div')
-        outerDiv.appendChild(contentCard)
-        contentCard.className = "card-content"
-        
-        h2tag = document.createElement('h2')
-        contentCard.appendChild(h2tag)
-        h2tag.className ="card-title"
-        h2tag.innerHTML = data['title']
-        
-        linknATag = document.createElement('a')
-        contentCard.appendChild(linknATag)
-        linknATag.className = 'button'
-        linknATag.href ="/info/index.html?type=anime&id="+ data['id']
-        linknATag.innerHTML ="Read More"
+        if (i!=10):
+            console.log('Creating item', i)
+            outerOuterDiv = document.getElementById('latest')
+            
+            outerDiv = document.createElement('div')
+            outerOuterDiv.appendChild(outerDiv)
+            outerDiv.className = "card popu"
+            
+            imageTag = document.createElement('img')
+            outerDiv.appendChild(imageTag)
+            imageTag.className="card-img"
+            imageTag.src= data['image']
+            
+            contentCard = document.createElement('div')
+            outerDiv.appendChild(contentCard)
+            contentCard.className = "card-content"
+            
+            h2tag = document.createElement('h2')
+            contentCard.appendChild(h2tag)
+            h2tag.className ="card-title"
+            h2tag.innerHTML = data['title']
+            
+            linknATag = document.createElement('a')
+            contentCard.appendChild(linknATag)
+            linknATag.className = 'button'
+            linknATag.href ="/info/index.html?type=anime&id="+ data['id']
+            linknATag.innerHTML ="Read More"
+            i=i+1
+        else:
+            break
 except():
     console.log("Error occured")
-# cardDiv = document.createElement('div')
-# outerDiv.appendChild(cardDiv)
-
-i=i+1
+    # cardDiv = document.createElement('div')
+    # outerDiv.appendChild(cardDiv)
