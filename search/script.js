1c750202dcd6a255dd112f404f63c5fde982931e
afnaan <afnaannim29@gmail.com>
2024-03-08 18:02:47 +0530

    lage

--- search/script.js
+++ search/script.js
@@ -1,47 +1,56 @@
 function tope(){
     window.scrollTo(0,0);
   }
-
-gsap.to("#nav", {
+  let mm = gsap.matchMedia();
+  mm.add("(min-width: 801)",()=>{
+    gsap.to(".counter", 0.25, {
+      delay: 1.5,
+      duration: 2,
+      opacity: 0,
+      ease: "power4.inOut"
+    });
+    
+    gsap.fromTo(".bar",{opacity:1},  {
+      delay: 2,
+      opacity: 0 ,
+      duration: 2,
+      ease: "power1.out",
+    
+    });
+  
+  })
+  let mobile = gsap.matchMedia();
+  mobile.add("(max-width: 800px)",()=>{
+  
+  gsap.to("#nav", {
     backgroundColor: "#0e100f",
     duration: 1.5,
     height: "80px",
+    marginTop: 0,
+    borderBottom: "1.5px solid #42433d",
     scrollTrigger: {
       trigger: "#nav",
       scroller: "body",
       // markers:true,
-      start: "740px",
+      start: "300px",
       // end: "top -11%",
       scrub: 1
     },
   });
-const btn = document.querySelector(".Nav_btn");
-btn.onmousemove = function(e){
-  const x = e.pageX - btn.offsetLeft;
-  const y = e.pageY - btn.offsetTop;
-
-  btn.style.setProperty("--x",x + "px");
-  btn.style.setProperty("--y",y + "px");
-}
-
-gsap.to(".counter", 0.25, {
-    delay: 3.5,
-    opacity: 0,
-    ease: "power4.inOut"
-  });
-  gsap.to(".bar", 1.5, {
-    delay: 3.5,
-    height: 0,
-    stagger: {
-      amount: 0.5,
-    },
-    ease: "power4.inOut"
+  gsap.fromTo(".nav-elem", { opacity: 0 }, {
+    opacity: 1,
+    duration: 3,
+    delay: 0,
+    stagger: 0.1,
+    scrub: 1
+  
+  })
   
   });
   setTimeout(() => {
     var a = document.getElementById("mkc");
-    a.style.zIndex = -100;
+    a.style.display = "none";
     var a = document.getElementById("mkc-2");
-    a.style.zIndex = -100;
+    a.style.display = "none";
     clearTimeout(setTimeout);
-  }, 5000);
+  }, 4000);
